d46259f0f2a4e1766aaf7b52280cee1d8af512b5
MTopOpt <59473102+MTopOpt@users.noreply.github.com>
2020-04-06 11:47:27 +0800

    Add files via upload

---
+++ MTO/2Dheatsink/src_TF/MTO_TF.C
@@ -0,0 +1,122 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "singlePhaseTransportModel.H"
+#include "turbulentTransportModel.H"
+#include "simpleControl.H"
+#include "fvOptions.H"
+#include <math.h>
+#include <fstream>
+#include <iostream>
+#include <iosfwd>
+#include <stdio.h>
+#include "petsc.h"
+#include "petscvec.h"
+#include <MMA.h>
+#include <mpi.h>
+
+template<class Type>
+void setCells
+(
+    GeometricField<Type, fvPatchField, volMesh>& vf,
+    const labelList& cells,
+    double value
+)
+{
+    forAll(cells, i)
+    {
+        vf[cells[i]] = value;
+    }
+}
+double fun(double gamma[],double del,double eta,int allcells);
+static char help[] = "topology optimization \n";
+int main(int argc, char *argv[])
+{
+    PetscInitialize(&argc,&argv,PETSC_NULL,help);
+    #include "postProcess.H"
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "createControl.H"
+    #include "createFields.H"
+    #include "readThermalProperties.H" 
+    #include "createFvOptions.H"
+    #include "initContinuityErrs.H"
+    #include "SIMP_initialize.H"
+    while (simple.loop())
+    {
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+        #include "primal_equation.H"
+        #include "adjoint_equation_T.H"
+        for(i=0;i<60;i++)
+        {
+           #include "adjoint_flow_U.H" 
+        }   
+        #include "costfunction.H"                           
+        #include "sensitivity.H"        
+        if(runTime.writeTime())
+        {
+        gamma.write();
+        //T.write();
+        //U.write();         
+        }
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+        << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+        << nl << endl;
+    }
+    delete mma;
+    PetscFinalize();
+    Info<< "End\n" << endl;
+    return 0;
+}
+
+
+//*********************************************************//function
+double fun(double gamma[],double del,double eta,int allcells)
+{
+     int i;
+     double z=0;
+     double *fg =new double[allcells];
+     
+     for(i=0;i<allcells;i++)
+     {
+        if(gamma[i]<=eta)
+        {
+          fg[i]=eta*(Foam::exp(-del*(1-gamma[i]/eta))-(1-gamma[i]/eta)*Foam::exp(-del));
+        }
+        else
+        {
+          fg[i]=eta+(1-eta)*(1-Foam::exp(-del*(gamma[i]-eta)/(1-eta))+(gamma[i]-eta)*Foam::exp(-del)/(1-eta));
+        }
+     }
+     for(i=0;i<allcells;i++)
+     {
+        z=z+gamma[i]-fg[i];
+     }
+     delete fg;
+     return {z};
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# MTO
+ MTO——Multiphysics Topology Optimization
+=========================================
+Description
+-----------
+The code presented is a parallel solver for multi-physics topology optimization on structured grids. Density-based method is adopted for solving 2D or 3D thermal-fluid-structural topology optimization problems based on OpenFOAM.  
+
+Installation
+------------
+Before running this solver, following softwares are needed.  
+(a) **OpenFOAM 5.0**  
+(b) **PETSc 3.12**  
+(c) **swak4Foam**  
+
+Parallel version of MMA written by Aage is also needed for updating the design variables (https://github.com/topopt/TopOpt_in_PETSc). We have changed this code to apply it in our solver. For simplicity, the C++ script have been compiled into a dynamic library (**libMMA_yu.so**), users should put this file in **FOAM_USER_LIBBIN** after installing OpenFOAM.  
+
+Run the solver
+--------------
+ After finishing above works, users should run **wmake** in the src folder, and then run **blockMesh** and **decomposePar** in the app folder. Finally, the multi-physics topology optimization solver can run in the app folder by **mpirun –n 20 MTO_solid –parallel**, where 20 is the default number of cores for parallel computing.
+ 
+Postplot
+--------
+After the optimization, users should run **reconstructPar** in the app folder and then run **paraFoam** to view the results with Paraview.  
+
+Solid displacement problem-2D  
+-----------------------------
+![image](https://github.com/Yuminghao-DLUT/MTO/blob/master/MTO/beam_2D.gif)  
+Solid displacement problem-3D  
+-----------------------------
+![image](https://github.com/Yuminghao-DLUT/MTO/blob/master/MTO/beam_3D.gif)  
+Thermal-fluid problem-2D  
+------------------------
+![image](https://github.com/Yuminghao-DLUT/MTO/blob/master/MTO/heatsink_2D.gif)  
+Thermal-fluid problem-3D  
+------------------------
+![image](https://github.com/Yuminghao-DLUT/MTO/blob/master/MTO/heatsink_3D.gif)  
